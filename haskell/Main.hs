7d760d40cf0b30fba3d6cc3c853518f76ab759a5
Simon Alling <alling.simon@gmail.com>
2019-02-15 15:21:37 +0100

    Add fibonacci benchmark
    The factorial benchmark turned out not to scale well at all, possibly
    due to the heap/memory becoming a bottleneck instead of raw computation.

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,15 +1,17 @@
+{-# LANGUAGE MagicHash #-}
+
 module Main where
 
+import GHC.Exts
 import Control.Parallel
 import Criterion.Main (defaultMain, bench, nf)
 import Control.Parallel.Strategies (parListChunk, rdeepseq, rseq, using)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 import Safe
-import Control.Monad (join)
 
 chunkSize :: Integral a => a
-chunkSize = 100
+chunkSize = 32
 
 chunks :: Integral a => a
 chunks = 8
@@ -20,26 +22,45 @@ factorials_seq = map fac
 factorials_par :: [Integer] -> [Integer]
 factorials_par xs = factorials_seq xs `using` parListChunk chunkSize rdeepseq
 
+fibonaccis_seq :: [Int] -> [Int]
+fibonaccis_seq = map fib
+
+fibonaccis_par :: [Int] -> [Int]
+fibonaccis_par xs = fibonaccis_seq xs `using` parListChunk chunkSize rdeepseq
+
 fac :: Integer -> Integer
 fac n | n <= 0 = 1
 fac n = n * fac (n - 1)
+
+-- Boxed ints:
+fib :: Int -> Int
+fib (I# n) = I# (fib' n)
+
+-- Unboxed ints:
+fib' :: Int# -> Int#
+fib' 0# = 0#
+fib' 1# = 1#
+fib' n = fib' (n -# 2#) +# fib' (n -# 1#)
 
 mainWith :: Integer -> IO ()
 mainWith bigNumber =
   let
     numberOfItems = chunkSize * chunks
     integers = replicate numberOfItems bigNumber
+    ints = map fromInteger integers
   in
     defaultMain
       [ bench "factorials_seq" (nf factorials_seq integers)
       , bench "factorials_par" (nf factorials_par integers)
+      , bench "fibonaccis_seq" (nf fibonaccis_seq ints)
+      , bench "fibonaccis_par" (nf fibonaccis_par ints)
       ]
 
 main :: IO ()
 main =
   let
     readMaybeInt = readMaybe :: String -> Maybe Integer
-    printHelp = putStrLn "Usage: ./Main 4000 factorials"
+    printHelp = putStrLn "Usage: ./Main 30 fibonaccis +RTS -H1G -A100M -N2"
   in do
     maybeBigNumber <- (readMaybeInt =<<) . headMay <$> getArgs
     maybe printHelp mainWith maybeBigNumber
--- haskell/benchmark.sh
+++ haskell/benchmark.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-readonly INT_SIZE=4000
+readonly INT_SIZE=30
 
 set -x
 
-make benchmark_seq
-
-./Main $INT_SIZE factorials_seq +RTS -H500M
-
 make benchmark_par
 
-./Main $INT_SIZE factorials_par +RTS -H500M -ls -N2
+./Main $INT_SIZE fibonaccis +RTS -H1G -A100M -N1
 
-threadscope Main.eventlog
+./Main $INT_SIZE fibonaccis +RTS -H1G -A100M -N4
