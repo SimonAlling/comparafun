eda92d79c981778a8fa86045ed088399e6e1f7d7
Simon Alling <alling.simon@gmail.com>
2019-03-20 15:50:13 +0100

    Generalize benchmark script

--- haskell/benchmark.sh
+++ haskell/benchmark.sh
@@ -1,12 +1,33 @@
 ***REMOVED***
+readonly MainFib="stack exec -- comparafun-fib"
 readonly INT_SIZE=30
-readonly Main="stack exec -- comparafun-fib"
+readonly MainKMeans="stack exec -- comparafun-kmeans"
+readonly K=8
 readonly RTS="-H1G -A100M"
 
 ***REMOVED***
 
 stack build
 
-$Main $INT_SIZE fibonaccis_par +RTS $RTS -N1
+function benchmarkFib() {
+	$MainFib $INT_SIZE fibonaccis_par +RTS $RTS -N1
+	$MainFib $INT_SIZE fibonaccis_par +RTS $RTS -N4
+}
 
-$Main $INT_SIZE fibonaccis_par +RTS $RTS -N4
+function benchmarkKMeans() {
+	$MainKMeans $K kmeans +RTS $RTS
+}
+
+function benchmarkAll() {
+	benchmarkFib
+	benchmarkKMeans
+}
+
+case $1 in
+"fib")
+	benchmarkFib ;;
+"kmeans")
+	benchmarkKMeans ;;
+"")
+	benchmarkAll ;;
+esac
