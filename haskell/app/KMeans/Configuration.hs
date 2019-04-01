e7451f63c13d4e000c6168cc37c060bd37928110
Simon Alling <alling.simon@gmail.com>
2019-04-01 09:19:12 +0200

    Add benchmark config message

--- haskell/app/KMeans/Configuration.hs
+++ haskell/app/KMeans/Configuration.hs
@@ -3,10 +3,18 @@
 module Configuration where
 
 import Prelude hiding ((>>))
+import Data.List (intersperse)
 import Testing (Seed, Size, K, generateTestData, runKmeans, Mode(..))
 import Algorithms.Lloyd.Strategies (Partitions(..))
 
 type Parameters = (Partitions, Seed, Size, K)
+
+data BenchmarkConfig = BenchmarkConfig
+  { ns :: [Int]
+  , ks :: [Int]
+  , seeds :: [Seed]
+  , parts :: [Int]
+  }
 
 printInfo :: Parameters -> IO ()
 printInfo (Partitions p, seed, n, k) =
@@ -17,3 +25,16 @@ printInfo (Partitions p, seed, n, k) =
     "partitions = " ++ show p
     []
     where (>>) = (:)
+
+printBenchmarkConfig :: BenchmarkConfig -> IO ()
+printBenchmarkConfig ps = putStrLn $ unlines $ do
+  "***** Benchmark configuration *****"
+  "  n:           " ++ showItems (ns ps)
+  "  k:           " ++ showItems (ks ps)
+  "  seed:        " ++ showItems (seeds ps)
+  "  partitions:  " ++ showItems (parts ps)
+  "***********************************"
+  [] where (>>) = (:)
+
+showItems :: Show a => [a] -> String
+showItems = concat . intersperse ", " . map show
