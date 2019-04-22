3bc1e499b8b15857d9a3be9464cecba54ec124b9
Simon Alling <alling.simon@gmail.com>
2019-04-22 19:29:33 +0200

    Misc cleanup

--- haskell/batch-kmeans.sh
+++ haskell/batch-kmeans.sh
@@ -1,24 +1,17 @@
 ***REMOVED***
 
 readonly now=$(date +"%Y-%m-%d_%H-%M")
-readonly LOG_FILE="benchmark-kmeans-$now.log"
-readonly cmd="stack exec -- comparafun kmeans batch +RTS -H1G -A100M"
-readonly collect="tee -a $LOG_FILE"
 
 stack build
 
-echo "$cmd" | $collect
-
-echo "**** 4 HECs ****"
-$cmd -N4  -RTS --output kmeans-4.html  | $collect
-
-echo "**** 10 HECs ****"
-$cmd -N10 -RTS --output kmeans-10.html | $collect
-
-echo "**** 20 HECs ****"
-$cmd -N20 -RTS --output kmeans-20.html | $collect
-
-echo "-------- DONE" | $collect
+for i in {1..20}
+do
+	cmd="stack exec -- comparafun kmeans batch +RTS -H1G -A100M -N$i -RTS --output kmeans-$i.html"
+	LOG_FILE="benchmark-kmeans-($now)-scaling-$i.log"
+	collect="tee -a $LOG_FILE"
+	echo "$cmd" | $collect
+	$cmd | $collect
+done
 
 readonly endTime=$(date +"%H:%M on %Y-%m-%d")
-echo "Finished at $endTime" | $collect
+echo "Finished at $endTime"
