7f9531b607cd85bf0de585a4adac0b13a1033fad
Simon Alling <alling.simon@gmail.com>
2019-04-24 15:09:33 +0200

    Add fib scaling data

--- haskell/src/Fib.hs
+++ haskell/src/Fib.hs
@@ -6,7 +6,7 @@ module Fib
     ) where
 
 import GHC.Exts -- Int# etc
-import Control.Parallel.Strategies (parMap, rdeepseq, using)
+import Control.Parallel.Strategies (parMap, rdeepseq, using, parListChunk)
 
 -- Boxed ("regular") ints:
 fib :: Int -> Int
@@ -21,5 +21,6 @@ fib' n = fib' (n -# 2#) +# fib' (n -# 1#)
 fibonaccis_seq :: [Int] -> [Int]
 fibonaccis_seq = map fib
 
-fibonaccis_par :: [Int] -> [Int]
-fibonaccis_par = parMap rdeepseq fib
+fibonaccis_par :: Int -> [Int] -> [Int]
+fibonaccis_par chunks xs = fibonaccis_seq xs `using` parListChunk chunkSize rdeepseq
+  where chunkSize = length xs `div` chunks
