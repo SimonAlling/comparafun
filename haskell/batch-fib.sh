7966c796ba1e3c4ba7bb3b68018dc45034efe8f6
Simon Alling <alling.simon@gmail.com>
2019-05-13 14:08:50 +0200

    Misc fib + GHC 8.6

--- haskell/batch-fib.sh
+++ haskell/batch-fib.sh
@@ -4,7 +4,7 @@ readonly now=$(date +"%Y-%m-%d_%H-%M")
 
 stack build
 
-for i in {1..4}
+for i in {1..20}
 do
 	cmd="stack exec -- comparafun fib batch +RTS -H1G -A100M -N$i -RTS --output fib-$i.html"
 	LOG_FILE="benchmark-fib-($now)-scaling-$i.log"
--- haskell/src/Fib.hs
+++ haskell/src/Fib.hs
@@ -3,10 +3,11 @@
 module Fib
     ( fibonaccis_seq
     , fibonaccis_par
+    , fibonaccis_parChunk
     ) where
 
 import GHC.Exts -- Int# etc
-import Control.Parallel.Strategies (parMap, rdeepseq, using, parListChunk)
+import Control.Parallel.Strategies (rdeepseq, using, parListChunk, parList)
 
 -- Boxed ("regular") ints:
 fib :: Int -> Int
@@ -21,6 +22,9 @@ fib' n = fib' (n -# 2#) +# fib' (n -# 1#)
 fibonaccis_seq :: [Int] -> [Int]
 fibonaccis_seq = map fib
 
-fibonaccis_par :: Int -> [Int] -> [Int]
-fibonaccis_par chunks xs = fibonaccis_seq xs `using` parListChunk chunkSize rdeepseq
-  where chunkSize = length xs `div` chunks
+fibonaccis_par :: [Int] -> [Int]
+fibonaccis_par xs = map fib xs `using` parList rdeepseq
+
+fibonaccis_parChunk :: Int -> [Int] -> [Int]
+fibonaccis_parChunk chunkSize xs = map fib xs `using` s
+  where s = parListChunk chunkSize rdeepseq
