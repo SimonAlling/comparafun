e8ecaf7ae0f513a1a2817254c824b282d011fbc5
Simon Alling <alling.simon@gmail.com>
2019-05-23 12:08:13 +0200

    Clean up Erlang code

--- erlang/fib.erl
+++ erlang/fib.erl
@@ -5,9 +5,9 @@ fib(0) -> 0;
 fib(1) -> 1;
 fib(N) -> fib(N-1) + fib(N-2).
 
-fibonaccis_seq(Xs) -> lists:map(fun(X) -> fib(X) end, Xs).
+fibonaccis_seq(Xs) -> lists:map(fun fib/1, Xs).
 
-fibonaccis_par(Xs) -> pmap(fun(X) -> fib(X) end, Xs).
+fibonaccis_par(Xs) -> pmap(fun fib/1, Xs).
 
 pmap(F, Xs) -> do
   , Parent = self()
