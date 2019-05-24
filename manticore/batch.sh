9a6d83f0bb1d21db317e497af7938ba17c8b1124
Simon Alling <alling.simon@gmail.com>
2019-05-24 13:53:02 +0200

    Improve Manticore batch script

--- manticore/batch.sh
+++ manticore/batch.sh
@@ -3,8 +3,14 @@
 readonly now=$(date +"%Y-%m-%d_%H-%M")
 readonly LOG_FILE_SEQ="fib-$now-seq.log"
 readonly LOG_FILE_PAR="fib-$now-par.log"
-readonly collect_seq="tee -a $LOG_FILE_SEQ"
-readonly collect_par="tee -a $LOG_FILE_PAR"
+
+collect_seq() {
+	head -2 | tail -1 | tee -a $LOG_FILE_SEQ
+}
+
+collect_par() {
+	head -2 | tail -1 | tee -a $LOG_FILE_PAR
+}
 
 readonly WIDTH=1000
 readonly DEPTH=30
@@ -12,21 +18,19 @@ readonly REPETITIONS=9
 
 echo "Compiling ..."
 pmlc -o fib fib.pml
-
-if [ $? -gt 0 ]; then
-	exit $?
-fi
-
+if [ $? -gt 0 ]; then exit $?; fi
 echo "Done."
 
 echo "Benchmarking sequential ..."
 for ((i=1; i<=REPETITIONS; i++)); do
 	echo "Repetition $i/$REPETITIONS ..."
-	{ time ./fib $WIDTH $DEPTH 1 ; } 2>&1 | head -2 | tail -1 | $collect_seq
+	{ time ./fib $WIDTH $DEPTH 1 ; } 2>&1 | collect_seq
 done
+sort -o $LOG_FILE_SEQ $LOG_FILE_SEQ
 
 echo "Benchmarking parallel ..."
 for ((i=1; i<=REPETITIONS; i++)); do
 	echo "Repetition $i/$REPETITIONS ..."
-	{ time ./fib $WIDTH $DEPTH 2 ; } 2>&1 | head -2 | tail -1 | $collect_par
+	{ time ./fib $WIDTH $DEPTH 2 ; } 2>&1 | collect_par
 done
+sort -o $LOG_FILE_PAR $LOG_FILE_PAR
